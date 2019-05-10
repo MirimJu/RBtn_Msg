987bc3f1a8e933a42c0723b1eae74a9c87a3e6e9
Mirim Ju <wnalfla@gmail.com>
2019-05-10 15:39:48 +0900

    #1 그룹박스, 라디오버튼, 버튼을 추가하여 폼 디자인 완성

--- RBtn_Msg/Form1.cs
+++ RBtn_Msg/Form1.cs
@@ -15,5 +15,15 @@ namespace RBtn_Msg
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radioButton3_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
     }
 }
